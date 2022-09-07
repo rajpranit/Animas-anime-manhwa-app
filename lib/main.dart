1d5049058ed7fe226d560245777352aeec2e371f
rajpranit <rajpranit1207@gmail.com>
2022-09-07 12:16:34 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animas',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animas Manhwa-Anime'),
+      ),
+    );
+  }
+}
